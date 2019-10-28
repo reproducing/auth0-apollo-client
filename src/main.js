70be8927ced2d487d8b1b8009075312dd679beb9
Francisco Lourenco <aristidesfl@gmail.com>
2019-10-28 16:10:50 +0100

    Add auth0

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createProvider } from './vue-apollo'
+import {Auth0Plugin} from './auth'
+
+// Install the authentication plugin
+Vue.use(Auth0Plugin, {
+	domain: 'apollo-example.auth0.com',
+	clientId: '9zVhwu7kAY1HtTCTbJY5LJHZRrTvKnwZ',
+	onRedirectCallback: appState => {
+		router.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname)
+	},
+})
 
 Vue.config.productionTip = false
 
